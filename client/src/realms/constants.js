8660ce984234f52a18167a40473d88b7f7843f1d
Aubrey Mae Mulawan <79576768+aubreymaemulawan@users.noreply.github.com>
2023-06-12 20:50:12 +0800

    Update constants.js

--- client/src/realms/constants.js
+++ client/src/realms/constants.js
@@ -1,4 +1,1 @@
-// Uncomment this to connect to my MongoDB Realm
-
-// export const APP_ID = "application-0-lafkj";
-export const APP_ID = "YOUR-APP-ID";
+export const APP_ID = "application-0-lafkj";
